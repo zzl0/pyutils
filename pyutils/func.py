2297ade07351a01eeb807c4c29d76189f35073f1
zzl <zhuzhaolong0@gmail.com>
2014-06-14 18:45:43 +0800

    add doctest to curry

--- pyutils/func.py
+++ pyutils/func.py
@@ -1,8 +1,15 @@
 # coding: utf-8
-import inspect
+import functools
 
 def curry(fn, argc=None):
     """A decorator curries the given function.
+
+    >>> @curry
+    ... def foo(a, b, c):
+    ...     return (a, b, c)
+    ...
+    >>> foo(1)(2)(3)
+    (1, 2, 3)
     """
     if argc is None:
         argc = fn.func_code.co_argcount
